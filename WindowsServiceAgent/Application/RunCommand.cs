e48150e4cbf933a0e90fcf962e66138400510f9c
Jeremy Wu <jeremy24wu@gmail.com>
2019-06-09 13:10:40 +0930

    Initial push

---
+++ WindowsServiceAgent/Application/RunCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace WindowsServiceAgent.Application
+{
+    public static class RunCommand
+    {
+        public static void TestCommand()
+        {
+            var processInfo = new ProcessStartInfo
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "CMD.exe",
+                Arguments = @"/C c:\temp\test.bat"
+            };
+
+            var process = Process.Start(processInfo);
+        }
+
+    }
+}
---
+++ WindowsServiceAgent/MyNewService.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using WindowsServiceAgent.Application;
+
+namespace WindowsServiceAgent
+{
+    public partial class MyNewService : ServiceBase
+    {
+        public MyNewService()
+        {
+            InitializeComponent();
+            eventLog1 = new EventLog();
+            
+            eventLog1.Source = "MyNewService";            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Your new my service is in OnStart.", EventLogEntryType.Information, 101, 1);
+
+            RunCommand.TestCommand();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
